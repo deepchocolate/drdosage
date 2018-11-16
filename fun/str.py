3e06136aec84f27e6ca9cfed8808d2d433848072
Andreas J <andreas@systerdyster.se>
2018-11-16 11:38:59 +0100

    Added simplify(txt, stripChars)

--- fun/str.py
+++ fun/str.py
@@ -2,3 +2,9 @@
 def rmExtraSpace(txt):
     while txt.find('  ') != -1: txt = txt.replace('  ', ' ')
     return txt
+
+def simplify(txt, stripChars='.?*=-+"!,'):
+    words = rmExtraSpace(txt).lower().split(' ')
+    o = ''
+    for w in words: o += w.strip(stripChars) + ' ' 
+    return rmExtraSpace(o.strip())
