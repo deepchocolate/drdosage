d598e9bf090402b15d079553d4b857d53970d2b8
Andreas J <andreas@systerdyster.se>
2019-02-01 12:09:22 +0100

    Draft funs

--- filter.py
+++ filter.py
@@ -3,16 +3,36 @@ from cla.fileio import *
 from fun import simplify
 import argparse, sys
 class Filter(FileIO):
-    
+    """
+		It is recommended to put the filtered output in a new column, i.e. append it
+		to a new column as the filtering may remove important semantics in the text.    
+    """
     def __init__(self, inputStream, outputStream, inputColumn, outputColumn=False):
+        # If not outputColumn, replace the data in this column.
         if outputColumn == False: outputColumn = inputColumn
         super(Filter,self).__init__(inputStream, outputStream)
         while next(self) != False:
-            self.setCol(outputColumn,simplify(self.get(inputColumn)))
+            self.setCol(outputColumn, simplify(self.get(inputColumn)))
             self.setOutput()
         self.writeOutput()
-    #def __init__(self, inputResource, outputResource, targetColumn=1):
-     #   print('hej')
+   
+	def removeExtraWhitespace(self):
+		"""
+		Collapse repeated whitespace into a single space ' '.
+		"""
+		pass
+
+	def polishWords(self):
+		"""
+		Remove leading/trailing characters in words such as periods, question marks etc.
+		"""
+		pass
+	
+	def filter(self):
+		"""
+		Apply the selected filters to the input data.
+		"""
+		pass
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
