9e1d926bba63c6c1e313f775b256ccf15c65012c
Andreas J <andreas@systerdyster.se>
2019-09-09 18:00:11 +0200

    Updated

--- cla/__init__.py
+++ cla/__init__.py
@@ -1,7 +1,8 @@
 #.. import fun
 #from .num import *
 #from .str import *
-from .classifier import Classifier
+from .classifier import *
+from .trainer import *
 from .transcript import Transcript
 from .transcripts import Transcripts
 from .fileio import FileIO
