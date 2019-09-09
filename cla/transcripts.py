cf68585ce9c88f9ebafe7d560b2b6c2682e5d2e5
Andreas J <andreas@systerdyster.se>
2019-09-09 17:39:25 +0200

    New draft of transcripts

--- cla/transcripts.py
+++ cla/transcripts.py
@@ -1,162 +1,259 @@
-#import num,str
-from .num import *
+from .fileio import *
+from fun.strman import rmExtraSpace
+from fun.num import isNumeric
 from .transcript import *
+SKIPCHARS = ')(.?*=-+"!,\n'
 class Transcripts:
-    def __init__(self,outputFormat = 'q,f,o'):
-        self.ignore = {}
-        #self.outputFormat = {x:0 for x in outputFormat.split(',')}
-        self.outputFormat = {'q':0, 'f':0, 'o':''}
-        # Container for manually classified strings
-        #self.classified = {}
-        self.ignoreChar = '/'
-        # Number of transcripts
-        self.n = 0
-        # Patterns
-        #self.patterns = {}
-        # Map from word to key, eg "tablett" -> O by its position in the sentence
-        # {1: { 'doser':'O', 'ett':'Q' } }
-        self.wordToKey = {}
-        # Translating pattern to values
-        # Ex: {'QO//': {'q':{ 'ett' : 1, 'två' : 2 }}}
-        # Ex: {1:{'q':{'en':1,'två':2},'o':{'tablett','t'}}}
-        self.keyToVal = {}
-        # Parsing functions
-        #self.parsers = {'q':self.parseQ,'o':self.parseO,'q-f':self.parseQF,'/':self.addIgnore}
-    
-    def getTranscript(self,txt,addVal=True,sep=' '):
-        s = ''
-        i = 0
-        #print(txt,self.wordToKey,self.keyToVal)
-        for word in txt.split(' '):
-            w = word.rstrip('.')
-            sym = self.getKey(w,i)
-            #print(sym)
-            s += sym
-            if addVal and sym != '/' and i in self.wordToKey and w in self.wordToKey[i] and self.wordToKey[i][w] in self.keyToVal[i] and w in self.keyToVal[i][self.wordToKey[i][w]]:
-                s += str(self.keyToVal[i][self.wordToKey[i][w]][w])
-            s += sep
-            i += 1
-        if s.strip('/ ') == '': return False
-        else: return s.strip(' ')
-    
-    def getTranslation(self, txt, instruction=False):
-        """
-        Get translation according to desired output format
-        """
-        if instruction == False: instruction = self.getTranscript(txt,False, '')
-        o = []
-        word = txt.split(' ')
-        for i in range(0,len(instruction)):
-        #for i,word in enumerate(txt.split(' ')):
-            w = word[i].rstrip('.')
-            #k = self.getKey(w, i)
-            k = instruction[i]
-            if k != '/':
-                v = self.getVal(w, i, k)
-                if isinstance(v, bool): raise Exception('Not able to translate!')
-                o += [(k, v)]
-        if len(o) > 0: return Transcript(o)
-        else: return False
-            
-    
-    def getInstruction(self,txt,pattern=False,error=False):
-        if pattern == False:
-            pattern = self.getTranscript(txt)
-            if pattern == False: pattern = '-'
-        if error: print('The instruction could not be parsed! Please try again!')
-        print('I could not understand some of this:')
-        print(txt)
-        print("Q [#] - Quantity\nO [name] - Object\n- to skip")
-        print('Suggested: '+pattern.upper())
-        inp = input("Enter details (leave blank to use suggestion): ")
-        inp = inp.lower().strip()
-        # Skip this text
-        if inp == '-': return False
-        elif len(inp) > 0:
-            try:
-                return self.parseInstruction(txt, inp)
-            except BaseException as e:
-                return self.getInstruction(txt, pattern, True)
-        # If input empty, take the suggestion
-        else: return self.getTranscript(txt, False, '')
-    
-    # Get key of word
-    def getKey(self,word, pos):
-        #print(word,pos)
-        if isNumeric(word): word = '#'
-        if pos in self.wordToKey and word in self.wordToKey[pos]:
-            return self.wordToKey[pos][word]
-        else: return '/'
-    
-    def getVal(self, word, pos, key):
-        if (key == 'q' or key == 'f') and isNumeric(word): return float(word)
-        if word in self.keyToVal[pos][key]: return self.keyToVal[pos][key][word]
-        else: return False
-    
-    def parseInstruction(self, text, instruction):
-        """
-        Parse instruction applied to text and return the raw instruction with values stripped
-        """
-        s = ''
-        textParts = text.split(' ')
-        i = 0
-        for word in instruction.split(' '):
-            if word != '/':
-                tp = textParts[i].rstrip('.')
-                # If word is not number, use instruction
-                #print(tp, word)
-                self.addWordToKey(i, tp, word[0])
-                # Single q in instruction implies word is numeric 
-                if len(word) > 1: self.addKeyToVal(i, tp, word[0], word[1:])
-                # Test the validity of text as numeric
-                elif word[0] != 'o' and word[0] != '/': float(tp)
-                #i += 1
-                s += word[0]
-            else:
-                s += '/'
-            i += 1
-        return s
-        
-    # Add a pattern txt to position pos (in sentence) of type obj
-    def addWordToKey(self,pos,txt, obj):
-        if isNumeric(txt): key = '#'
-        else: key = txt
-        if pos in self.wordToKey:
-            if key in self.wordToKey[pos] and self.wordToKey[pos][key] != obj: print('Replacement of object '+self.wordToKey[pos][key]+' with '+obj+'; key '+key+' at position '+str(pos))
-            self.wordToKey[pos][key] = obj
-        else: self.wordToKey[pos] = {key:obj}
+	"""
+	All text and patterns used as input is assumed to have been filtered and is clean.
+	(1) Add text with accompanying transcript.
+	(2) Store word<->qfot pairs
+	(3) Create matrixes
+	"""
+	def __init__(self):
+		# Words to numbers, e.g. five=5
+		self._wordToNumeric = {}
+		# Word to assigned value, eg. tablet or pill=pill
+		self._wordToVal = {}
+		self._wordToTime = {}
+		self._ignoreChar = '/'
+		self._numberChar = '#'
+		self._funs = {'q':self.addWordToNumeric,'f':self.addWordToNumeric,'o':self.addWordToVal,'t':self.addTimeObject,'s':self.skip,'/':self.skip, ';':self.split,'*':self.setOperator}
+		self._timeValues = {'d':1,'w':7}
+		self._skips = 0
+	
+	def getObjectData(self):
+		return {
+			'wordToNumeric':self._wordToNumeric,
+			'wordToVal':self._wordToVal,
+			'wordToTime':self._wordToTime,
+			'ignoreChar':self._ignoreChar,
+			'numberChar':self._numberChar,
+			'timeValues':self._timeValues
+			}
+	
+	def setObjectData(self, data):
+		self._wordToNumeric = data['wordToNumeric']
+		self._wordToVal = data['wordToVal']
+		self._wordToTime = data['wordToTime']
+		self._ignoreChar = data['ignoreChar']
+		self._numberChar = data['numberChar']
+		self._timeValues = data['timeValues']
+		return self
 
-    # key: key of pattern. txt: word to translate. obj: object type. val: what object
-    def addKeyToVal(self,pos,txt,obj,val):
-        if obj == 'f' or obj == 'q': val = float(val)
-        if not pos in self.keyToVal: self.keyToVal[pos] = {obj : {txt:val}}
-        elif not obj in self.keyToVal[pos]: self.keyToVal[pos][obj] = {txt:val}
-        else: self.keyToVal[pos][obj][txt] = val
+	def encode(self, text, pattern):
+		"""
+		Encode a text with a corresponding pattern.
+		"""
+		i = 0
+		#txtParts = text.split(' ')
+		txtParts = text
+		pattern = rmExtraSpace(pattern.lower().strip())
+		if pattern == '': pattern = 's'+str(len(txtParts))
+		else: pattern = expandSkips(pattern)
+		parts = pattern.split(' ')
+		# Add up word codings
+		for part in parts:
+			if len(part) > 1 and part[1] == 'w' and text[i] == 'sedan':
+				print(text)
+				quit()
+			if not part[0] in self._funs: raise KeyError(part[0] + ' is an unrecognized operator!')
+			if not part in '*;':
+				self._funs[part[0]](txtParts[i], part[1:])
+				i += 1
+		return self
+	
+	def decode(self, text, pattern, brute=True):
+		"""
+		Decode a text using a transcript.
+		"""
+		self._transcriptN = 0
+		transcripts = []
+		patterns = expandSkips(pattern).lower().split(';')
+		if brute == True:
+			patterns = patterns[0].split(' ')
+			new = []
+			newPat = []
+			t = 0
+			for x in patterns:
+				if x == 't': t += 1
+				new += [x]
+				if t == 2:
+					t = 0
+					newPat += [' '.join(new)]
+					new = []
+			if len(new) != 0: newPat += [' '.join(new)]
+			patterns = newPat
+		i = 0
+		txtParts = text.split(' ')
+		for transcript in patterns:
+			novelObjects = 0
+			transcript = transcript.strip()
+			#print(transcript)
+			codings = []
+			for part in transcript.split(' '):
+				if part in 'qft':
+					if isNumeric(txtParts[i]): codings += [(part,float(txtParts[i]))]
+					elif part in 'qf': codings += [(part,self.wordToNumeric(txtParts[i]))]
+					elif part == 't': codings += [(part,self.wordToTime(txtParts[i]))]
+				elif part == 'o':
+					obj = self.wordToVal(txtParts[i])
+					if obj == False:
+						obj = txtParts[i]
+						novelObjects += 1
+					codings += [(part,obj)]
+				elif part == '*': codings += [('*',None)]
+				if part != '*': i += 1
+				#if not part in '*;': i += 1
+			self._transcriptN += 1
+			if len(codings) > 0:
+				tra = Transcript2(codings)
+				tra.setNovelObjects(novelObjects)
+				transcripts += [tra]
+		return transcripts
+	
+	def skip(self, word, operator):
+		"""
+		if n:
+			if not isNumeric(n): raise ValueError('Character ' + word + ' coded as numeric ' + n + ' does not translate to numeric!')
+			self._skips = int(n)
+		self._skips -= 1
+		return self._skips
+		"""
+		return word
+	
+	def split(self, pattern):
+		"""
+		Splits text and transcripts if there are multiple
+		"""
+		transcripts = []
+		transcript = ''
+		#txtParts = text.split(' ')
+		i = 0
+		for part in pattern.split(' '):
+			if part == ';':
+				transcripts += [transcript.rstrip()]
+				transcript = ''
+			elif part != '*':
+				transcript += txtParts[i] + ' '
+				i += 1
+		if transcript != '': transcripts += [transcript]
+		return transcripts
+				
+	
+	def setOperator(self, word, operator):
+		pass
+	
+	def wordToNumeric(self, word):
+		if not word in self._wordToNumeric: raise ValueError('Word '+word+' does not have a numeric translation!')
+		return self._wordToNumeric[word]
+	
+	def addWordToNumeric(self, word, number):
+		"""
+		For (q)uantity/(f)requency types. As all numbers are internally coded as #(.#)
+		there's no need to add the numeric.
+		"""
+		if number:
+			if not isNumeric(number): raise ValueError('Word ' + word + ' coded as numeric ' + number + ' does not translate to numeric!')
+			self._wordToNumeric[word] = float(number)
+	
+	def wordToVal(self, word):
+		return self._wordToVal[word] if word in self._wordToVal else False
+	
+	def addWordToVal(self, word, val):
+		"""
+		For (o)bject types.
+		"""
+		if val == '': raise ValueError(word + ' was assigned an empty value!')
+		if word in self._wordToVal and self._wordToVal[word] != val: raise Exception(word + '=' + val + ' replaced ' + self._wordToVal[word])
+		self._wordToVal[word] = val
+	
+	def wordToTime(self, word):
+		if word in self._wordToTime: return self._wordToTime[word]
+		else: return self.wordToNumeric(word)
+	
+	def addTimeObject(self, word, val):
+		"""
+		For (t)ime types.
+		"""
+		if val:
+			if isNumeric(val): self.addWordToNumeric(word, val)#self._wordToNumeric[word] = val
+			else: self._wordToTime[word] = val
+	
+"""	
+	def __iter__(self):
+		self._position = 0
+		return self
+
+	def __next__(self):
+		self._position += 1
+		if self._position > self.n: raise StopIteration
+		return Transcript
     
-    def addIgnore(self,pos,word):
-        if not pos in self.ignore: self.ignore[pos] = [word]
-        elif not word in self.ignore[pos]: self.ignore[pos] += [word]
-    
-    def getObjectData(self):
-        return {'wordToKey':self.wordToKey, 'keyToVal':self.keyToVal}
-    
-    def setObjectData(self,data):
-        """
-        Data should be dict as returned by getObjectData
-        """
-        #print(data['wordToKey'])
-        #data['wordToKey'] = {int(k):v for k,v in data['wordToKey'].items()}
-        #print(data['wordToKey'])
-        #quit()
-        # Annoyingly, using JSON, keys have to be converted back to integer in map
-        self.wordToKey = {int(k):v for k,v in data['wordToKey'].items()}
-        self.keyToVal = {int(k):v for k,v in data['keyToVal'].items()}
-        #print(self.wordToKey)
-        #print(self.keyToVal)
-        #quit()
-        #self.wordToKey = data['wordToKey']
-        #self.keyToVal = data['keyToVal']
-        return self
-    
-    def toJSON(self):
-        return json.dumps(self.getObjectData())
+	def rewind(self):
+		self.position = 0
+		return self
+"""
+
+"""
+if __name__ == '__main__':
+	t = Transcripts()
+	s = Sequences(6,4, False)
+	#t.addTrancript('one pill per day week 1 2 pills per day weeks two','q1 opill s2 tw t q opill s2 tw t2')
+	#t._funs['q']('two','2')
+	dire = '../../csv/'
+	f = FileIO(open(dire+'dispensations_training_sample.csv'))
+	output = 'id,time,dose\n'
+	i = 0;
+	while next(f) != False:
+		print('Record: '+str(i)+' '+f.get('pattern'))
+		t.addTrancript(f.get('doser'), f.get('pattern'))
+		if f.get('pattern') != '':
+			pattern = stripArguments(f.get('pattern').lower().replace(' ;','').replace(' *',''))
+			s.add(f.get('doser'), pattern)
+			for x in t.getTranscripts(f.get('doser'),stripArguments(f.get('pattern').lower())):
+				output += f.get('id') + ','+str(x.timePoint()) + ','+str(x.total())+'\n'
+			#print(stripArguments(f.get('pattern')))
+		#print(str(i)+': '+f.get('doser'))
+		i += 1
+	open(dire+'testoutput.csv','w').write(output)
+	fit = s.fit()
+	s.save(open('../seqModel.model','w'))
+	f.rewind()
+	correctN = 0
+	N = 0
+	errors = ''
+	s = Sequences()
+	s.load(open('../seqModel.model'))
+	while next(f) != False:
+		if f.get('pattern') != '':
+			N += 1
+			cleanInput = stripArguments(f.get('pattern').lower().replace(' ;','').replace(' *','').replace('/','s'))
+			cleanOutput = ' '.join(s.predictSequence(f.get('doser')))
+			print(f.get('doser')+': ' + cleanInput + ' : ' + cleanOutput)
+			if cleanInput.replace(' ','').rstrip('s') == cleanOutput.replace(' ', '').rstrip('s'): correctN += 1
+			else: errors += f.get('doser')+': ' + cleanInput + ' : ' + cleanOutput + "\n"
+	print('ERRORS')
+	print(errors)
+	print(correctN, N)
+	wrds = ''
+	for word,n in s.getWordCount().items(): wrds += word +': ' + str(n) + "\n"
+	#open(dire+'wordcount.txt','w').write(wrds)
+	#print(s.getWordCount())
+	#print(s.predictSequence('1 tablett till kvällen för sömn och minskad oro'))
+	#print(t._wordToNumeric)
+	#print(t._wordToTime)
+"""
+"""
+one pill per day
+q o s s
+q = o s one pill
+o = s s pill per
+s = s per day
+
+Prediction
+OP1 = one pill
+OP2 = OP1 pill per
+OP3 = OP1 OP2 per day
+OP4 = OP2 OP3 day
+"""
